d861257bca3b9beaa4cce57bb76dc37d96086cfb
Anderson Lopes <anderson.lopes.2010@gmail.com>
2017-07-10 20:45:03 -0300

    Tipagem dinâmica.

---
+++ tipagem_dinamica.js
@@ -0,0 +1,11 @@
+var idade = 25;
+console.log(idade);
+console.log(typeof idade);
+
+idade += ' anos.'; 
+console.log(idade);
+console.log(typeof idade);
+
+idade = false; 
+console.log(idade);
+console.log(typeof idade);
