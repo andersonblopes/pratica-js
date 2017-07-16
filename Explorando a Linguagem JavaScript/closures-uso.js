96bf43b21d1cab5ac66bc05bbae9acdf0d41531f
Anderson Lopes <anderson.lopes.2010@gmail.com>
2017-07-16 00:00:25 -0300

    closures e funções autoexecutáveis 2.

--- Explorando a Linguagem JavaScript/closures-uso.js
+++ Explorando a Linguagem JavaScript/closures-uso.js
@@ -21,3 +21,67 @@ var incrementar2 = (function(){
 console.log("output", incrementar2());
 console.log("output", incrementar2());
 console.log("output", incrementar2());
+
+
+
+/*Exemplo 1 */
+var calculaPreco = function (quantidade, preco ) {
+	var quantidade = quantidade;
+	var preco = preco;
+	
+	return function () {
+		return quantidade * preco;
+	}();
+};
+console.log(calculaPreco(5,100.50));
+
+
+/*Exemplo 2 */
+var calculaPreco = function (quantidade, preco ) {
+	var quantidade = quantidade;
+	var preco = preco;
+	
+	return function () {
+		return quantidade * preco;
+	};
+};
+console.log(calculaPreco(5,100.50)());
+
+
+/*Exemplo 3 */
+var calculaPreco = function () {
+	var quantidade = 5;
+	var preco = 100.5;
+	var valorTotal = quantidade * preco;
+	
+	return function () {
+		return valorTotal;	
+	}();
+}();
+console.log(calculaPreco);
+
+
+/*Exemplo 4 */
+var calculaPreco = function () {
+	var quantidade = 5;
+	var preco = 100.5;
+	var valorTotal = quantidade * preco;
+	
+	return function () {
+		return valorTotal;	
+	};
+}();
+console.log(calculaPreco());
+
+
+/*Exemplo 5 */
+var calculaPreco = function () {
+	var quantidade = 5;
+	var preco = 100.5;
+	var valorTotal = quantidade * preco;
+	
+	return function () {
+		return valorTotal;	
+	}();
+};
+console.log(calculaPreco());
