dcb55ab40de770527860bb7d0ccd2c59f5fd4d74
Anderson Lopes <anderson.lopes.2010@gmail.com>
2017-07-16 00:50:29 -0300

    Criação de objetos dentro de objetos 2.

--- Explorando a Linguagem JavaScript/compondo-objetos-com-objetos.js
+++ Explorando a Linguagem JavaScript/compondo-objetos-com-objetos.js
@@ -17,4 +17,6 @@ console.log('Cidade que o helena mora', helena.endereco.cidade);
 helena.endereco.numero = 1305
 console.log(helena);
 console.log(helena.endereco.cep); // Undefined pois cep não existe no objeto endereço
+helena.endereco.cep = '60.831-222'
+console.log(helena.endereco.cep);
 console.log('Estado', helena['endereco']['estado']); // Acesso dinâmico a atributos
